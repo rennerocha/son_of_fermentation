47b4b0c911d30af3c4a31aad62259e66965fa5be
Renne Rocha <rrocha@mwneo.com>
2016-12-30 11:12:12 -0200

    use env variables to get mqtt broker username and password

--- client/client.py
+++ client/client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import os
 from threading import Thread
 
 import chamber
@@ -8,8 +9,8 @@ import son_of_fermentation
 
 MQTT_BROKER_HOST = 'broker.myownbrewery.com'
 MQTT_BROKER_PORT = 8883
-MQTT_BROKER_USERNAME = 'xxx'
-MQTT_BROKER_PASSWORD = 'xxx'
+MQTT_BROKER_USERNAME = os.environ.get('MQTT_BROKER_USERNAME', '')
+MQTT_BROKER_PASSWORD = os.environ.get('MQTT_BROKER_PASSWORD', '')
 
 
 def main():
@@ -24,7 +25,7 @@ def main():
 
     sof_thread = Thread(target=son_of_fermentation.start, args=(mqtt_settings, ))
     sof_thread.start()
-    
+
     chamber_thread = Thread(target=chamber.start, args=(mqtt_settings, ))
     chamber_thread.start()
 
